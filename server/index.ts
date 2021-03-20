65b6cce17c1bfcba29eb8eb3d2b3dd21c604593b
Nathan Jones <njones@stridenyc.com>
2021-03-20 11:57:16 -0400

    Format server code

--- server/index.ts
+++ server/index.ts
@@ -1,63 +1,72 @@
-import 'newrelic'
-import db from './config';
-import express from 'express';
-import mqtt from 'mqtt';
-import path from 'path';
+import "newrelic";
+import db from "./config";
+import express from "express";
+import mqtt from "mqtt";
+import path from "path";
 
 const client = mqtt.connect(process.env.CLOUDMQTT_URL);
 
-const DOORBELL_ACTIVE = 'doorbell/active';
-const DOORBELL_INACTIVE = 'doorbell/inactive';
-const DOORBELL_BUZZER = 'doorbell/buzzer';
+const DOORBELL_ACTIVE = "doorbell/active";
+const DOORBELL_INACTIVE = "doorbell/inactive";
+const DOORBELL_BUZZER = "doorbell/buzzer";
 
-client.on('connect', () => {
-  client.subscribe([DOORBELL_ACTIVE, DOORBELL_INACTIVE]);
-})
+client.on("connect", () => {
+  client.subscribe([
+    DOORBELL_ACTIVE,
+    DOORBELL_INACTIVE,
+    DOORBELL_BUZZER,
+  ]);
+});
 
-client.on('message', (topic, message) => {
-  console.log('received message %s %s', topic, message)
+client.on("message", (topic, message) => {
+  console.log("received message %s %s", topic, message);
   switch (topic) {
     case DOORBELL_ACTIVE:
-      db.query(
-        'INSERT INTO events (status, created_at) VALUES ($1, $2)',
-        ['active', new Date().toISOString()],
-      );
+      db.query("INSERT INTO events (status, created_at) VALUES ($1, $2)", [
+        "active",
+        new Date().toISOString(),
+      ]);
       break;
     case DOORBELL_INACTIVE:
-      db.query(
-        'INSERT INTO events (status, created_at) VALUES ($1, $2)',
-        ['inactive', new Date().toISOString()],
-      );
+      db.query("INSERT INTO events (status, created_at) VALUES ($1, $2)", [
+        "inactive",
+        new Date().toISOString(),
+      ]);
       break;
     default:
-      console.log('No handler for topic %s', topic)
+      console.log("No handler for topic %s", topic);
   }
-})
+});
 
-const app = express()
+const app = express();
 
-app.get('/timestamps', (req, res) => {
-  db.query("SELECT created_at FROM events WHERE status = 'active' ORDER BY created_at desc", (error, results) => {
-    res.status(200).json(results?.rows.map((row) => row.created_at));
-  });
-})
+app.get("/timestamps", (req, res) => {
+  db.query(
+    "SELECT created_at FROM events WHERE status = 'active' ORDER BY created_at desc",
+    (error, results) => {
+      res.status(200).json(results?.rows.map((row) => row.created_at));
+    }
+  );
+});
 
-app.get('/status', (req, res) => {
-  db.query('SELECT status FROM events ORDER BY created_at desc LIMIT 1', (error, results) => {
-    res.status(200).json(results?.rows.map((row) => row.status)[0]);
-  });
-})
+app.get("/status", (req, res) => {
+  db.query(
+    "SELECT status FROM events ORDER BY created_at desc LIMIT 1",
+    (error, results) => {
+      res.status(200).json(results?.rows.map((row) => row.status)[0]);
+    }
+  );
+});
 
-app.post('/buzzer', (req, res) => {
-  client.publish(DOORBELL_BUZZER, 'test');
+app.post("/buzzer", (req, res) => {
+  client.publish(DOORBELL_BUZZER, "test");
   res.status(201).json();
-})
+});
 
-const staticFiles = express.static(path.join(__dirname, '../client/build'))
+const staticFiles = express.static(path.join(__dirname, "../client/build"));
 
-app.use(staticFiles)
+app.use(staticFiles);
 
-app.use('/*', staticFiles)
+app.use("/*", staticFiles);
 
-app.listen(process.env.PORT || 3001,
-  () => console.log("Server is running..."));
+app.listen(process.env.PORT || 3001, () => console.log("Server is running..."));
