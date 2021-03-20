f8b02a59b5df100dc358e12f642177bb5a0e06d3
Nathan Jones <njones@stridenyc.com>
2021-03-20 10:50:23 -0400

    Move formatting code

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import * as ReactDOM from 'react-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import moment from "moment-timezone";
-import { Alert, AlertTitle } from '@material-ui/lab';
-import { Button, List, ListItemText, Container } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import * as ReactDOM from "react-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import { Alert, AlertTitle } from "@material-ui/lab";
+import { Button, List, ListItemText, Container } from "@material-ui/core";
+import { formatTimestamp } from "./utils";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useInterval = (callback, delay) => {
   const savedCallback = React.useRef();
@@ -22,78 +22,75 @@ const useInterval = (callback, delay) => {
     let id = setInterval(tick, delay);
     return () => clearInterval(id);
   }, [delay]);
-}
+};
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(2),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'start',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "start",
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
 }));
-
-const formatTimestamp = (timestamp) => {
-  const date = moment(timestamp);
-  const today = moment().startOf('day')
-  const yesterday = moment().subtract(1, 'day').startOf('day')
-
-  if (date > today) return `Today ${date.format('h:mm:ss a')}`
-  if (date > yesterday) return `Yesterday ${date.format('h:mm:ss a')}`
-  return date.format('MMMM Do YYYY, h:mm:ss a');
-}
-
 const Doorbell = () => {
   const [timestamps, setTimestamps] = React.useState([]);
-  const [status, setStatus] = React.useState('');
+  const [status, setStatus] = React.useState("");
   const isInactive = status !== "active";
 
   useInterval(async () => {
-    const response = await fetch('/timestamps')
-    const data = await response.json()
-    setTimestamps(data)
+    const response = await fetch("/timestamps");
+    const data = await response.json();
+    setTimestamps(data);
   }, 2000);
 
   useInterval(async () => {
-    const response = await fetch('/status')
-    const data = await response.json()
-    setStatus(data)
+    const response = await fetch("/status");
+    const data = await response.json();
+    setStatus(data);
   }, 2000);
 
-  const openDoor = () => fetch('/buzzer', { method: 'POST' });
+  const openDoor = () => fetch("/buzzer", { method: "POST" });
   const classes = useStyles();
 
   return (
     <div>
-      <Alert severity={isInactive ? 'info' : 'success'}>
-        <AlertTitle>Device Status: {isInactive ? 'INACTIVE' : 'ACTIVE'}</AlertTitle>
+      <Alert severity={isInactive ? "info" : "success"}>
+        <AlertTitle>
+          Device Status: {isInactive ? "INACTIVE" : "ACTIVE"}
+        </AlertTitle>
       </Alert>
       <Container maxWidth="md" component="main">
         <CssBaseline />
-        {!isInactive && <Button fullWidth variant="contained" color="primary" className={classes.submit} onClick={openDoor}>
-          Buzzer
-        </Button>
-        }
+        {!isInactive && (
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={openDoor}
+          >
+            Buzzer
+          </Button>
+        )}
         <div className={classes.paper}>
           <section>
             <Typography component="h3" variant="h5">
               Activation times
             </Typography>
             <List>
-              {timestamps.map(timestamp => (
+              {timestamps.map((timestamp) => (
                 <ListItemText key={timestamp}>
                   {formatTimestamp(timestamp)}
                 </ListItemText>
-              )
-              )}
+              ))}
             </List>
           </section>
         </div>
-      </Container >
+      </Container>
     </div>
   );
-}
-ReactDOM.render(<Doorbell />, document.getElementById('root'))
+};
+ReactDOM.render(<Doorbell />, document.getElementById("root"));
---
+++ client/src/utils.js
@@ -0,0 +1,11 @@
+import moment from "moment-timezone";
+
+export const formatTimestamp = (timestamp) => {
+  const date = moment(timestamp);
+  const today = moment().startOf("day");
+  const yesterday = moment().subtract(1, "day").startOf("day");
+
+  if (date > today) return `Today ${date.format("h:mm:ss a")}`;
+  if (date > yesterday) return `Yesterday ${date.format("h:mm:ss a")}`;
+  return date.format("MMMM Do YYYY, h:mm:ss a");
+};
