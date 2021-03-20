10cd67e7323091adf474b2418a3d2752e5ef0eb2
Nathan Jones <njones@stridenyc.com>
2021-03-20 12:12:31 -0400

    Add battery API - returns 20%

--- server/index.ts
+++ server/index.ts
@@ -9,28 +9,36 @@ const client = mqtt.connect(process.env.CLOUDMQTT_URL);
 const DOORBELL_ACTIVE = "doorbell/active";
 const DOORBELL_INACTIVE = "doorbell/inactive";
 const DOORBELL_BUZZER = "doorbell/buzzer";
+const BATTERY_STATUS = "battery/status";
 
 client.on("connect", () => {
   client.subscribe([
     DOORBELL_ACTIVE,
     DOORBELL_INACTIVE,
     DOORBELL_BUZZER,
+    BATTERY_STATUS,
   ]);
 });
 
+enum DeviceStatus {
+  ACTIVE = "active",
+  INACTIVE = "inactive",
+}
+
 client.on("message", (topic, message) => {
   console.log("received message %s %s", topic, message);
+  const now = new Date().toISOString();
   switch (topic) {
     case DOORBELL_ACTIVE:
       db.query("INSERT INTO events (status, created_at) VALUES ($1, $2)", [
-        "active",
-        new Date().toISOString(),
+        DeviceStatus.ACTIVE,
+        now,
       ]);
       break;
     case DOORBELL_INACTIVE:
       db.query("INSERT INTO events (status, created_at) VALUES ($1, $2)", [
-        "inactive",
-        new Date().toISOString(),
+        DeviceStatus.INACTIVE,
+        now,
       ]);
       break;
     default:
@@ -40,22 +48,30 @@ client.on("message", (topic, message) => {
 
 const app = express();
 
-app.get("/timestamps", (req, res) => {
+app.get("/timestamps", (_req, res) => {
   db.query(
     "SELECT created_at FROM events WHERE status = 'active' ORDER BY created_at desc",
-    (error, results) => {
-      res.status(200).json(results?.rows.map((row) => row.created_at));
+    (_error, results) => {
+      res.status(200).json(results?.rows.map((row) => row.created_at) || []);
     }
   );
 });
 
-app.get("/status", (req, res) => {
+app.get("/status", (_req, res) => {
   db.query(
     "SELECT status FROM events ORDER BY created_at desc LIMIT 1",
-    (error, results) => {
-      res.status(200).json(results?.rows.map((row) => row.status)[0]);
+    (_error, results) => {
+      res
+        .status(200)
+        .json(
+          results?.rows.map((row) => row.status)[0] || DeviceStatus.INACTIVE
+        );
     }
   );
+});
+
+app.get("/battery", (_req, res) => {
+  res.status(200).json("20");
 });
 
 app.post("/buzzer", (req, res) => {
